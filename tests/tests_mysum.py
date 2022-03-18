387ad1ce42cb74c061ccab99f97b08688fd89a6e
Arndt <arndt_simon@web.de>
2022-03-18 21:02:36 +0100

    fix test

--- tests/tests_mysum.py
+++ tests/tests_mysum.py
@@ -4,7 +4,7 @@ from  randomFunktions import my_sum
 class TestMyTry(unittest.TestCase):
 
     def testSumEasy(self):
-        self.assertEqual(my_sum.sum([2,5]), 5)
+        self.assertEqual(my_sum.sum([2,5]), 7)
     
     def test_bad_type(self):
         data = "bananana"
