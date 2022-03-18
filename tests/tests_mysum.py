ed851b50fda62bb1df5e9094e27d975a40907034
Arndt <arndt_simon@web.de>
2022-03-18 11:07:41 +0100

    Initial commit

---
+++ tests/tests_mysum.py
@@ -0,0 +1,17 @@
+import unittest
+from  randomFunktions import my_sum
+
+class TestMyTry(unittest.TestCase):
+
+    def testSumEasy(self):
+        self.assertEqual(my_sum.sum([2,5]), 7)
+    
+    def test_bad_type(self):
+        data = "bananana"
+        with self.assertRaises(TypeError):
+            result = my_sum.sum(data)
+            print(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
