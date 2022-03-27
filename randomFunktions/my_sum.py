b8bea6022d6a46df76f9235850cf9755dc299c55
Arndt <arndt_simon@web.de>
2022-03-27 15:24:55 +0200

    Create commentary for doxygen

--- randomFunktions/my_sum.py
+++ randomFunktions/my_sum.py
@@ -1,5 +1,15 @@
-def sum(arg):
-    total = 0
-    for val in arg:
-        total += val
-    return total
+
+## @class MyMathOperations
+# @brief Class for self implemented mathematical functions.
+# 
+# This class provides several functions to execute mathematical operations
+class MyMathOperations():
+    
+    ## @brief Sums up elemnts in arg
+    # @param arg list containing float or int values to be summed up
+    # @return float or int over values of arg
+    def sum(arg):
+        total = 0
+        for val in arg:
+            total += val
+        return total
--- tests/tests_mysum.py
+++ tests/tests_mysum.py
@@ -1,12 +1,19 @@
 import unittest
-from randomFunktions import my_sum
+from randomFunktions import *
+from randomFunktions.my_sum import MyMathOperations
 
-
+## @class TestMyTry
+# @brief Class for all unittests.
+# 
+# This class provides several unittests for testing
+# each unit of the created code.
 class TestMyTry(unittest.TestCase):
 
+    ## @brief tests easy summation
     def testSumEasy(self):
-        self.assertEqual(my_sum.sum([2, 5]), 7)
+        self.assertEqual(MyMathOperations.sum([2, 5]), 7)
 
+    ## @brief tests bad input types
     def test_bad_type(self):
         data = "bananana"
         with self.assertRaises(TypeError):
